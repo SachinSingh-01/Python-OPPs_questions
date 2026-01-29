ca20db727eaa531b93fe6d69992f60d1f61cb7aa
Sachin Singh <sachinsingh19012@gmail.com>
2026-01-29 18:34:34 +0530

    Practice question

---
+++ 02_problem.py
@@ -0,0 +1,8 @@
+# Create a class Car with a constructor that takes brand and model and prints them.
+class Car:
+    brand="Toyota"
+    model="Fortuner"
+s1=Car()
+s2=Car()
+print("Brand of car:",s1.brand)
+print("Model of car:",s2.model)
