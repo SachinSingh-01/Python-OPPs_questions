e6a56bcb8ee437045a52e3fdfe27aa5d89f8e6a9
Sachin Singh <sachinsingh19012@gmail.com>
2026-01-30 20:34:10 +0530

    question

---
+++ 03_problem.py
@@ -0,0 +1,8 @@
+# Make a class Person with variables name and age. Create two objects with different value
+class person:
+    name="Sachin Singh"
+    age=11
+n=person()
+a=person()
+print("Name of the person:",n.name)
+print("Age of the person:",a.age)
